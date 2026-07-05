9028018733bac2b247912f113fb3760631ed08cf
Adenpearce123 <143508040+Adenpearce123@users.noreply.github.com>
2026-07-05 18:18:06 +0700

    Add files via upload

---
+++ db_bimba (1).sql
@@ -0,0 +1,51 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Waktu pembuatan: 05 Jul 2026 pada 13.17
+-- Versi server: 10.4.32-MariaDB
+-- Versi PHP: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `db_bimba`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `kelas`
+--
+-- Kesalahan membaca struktur untuk tabel db_bimba.kelas: #1932 - Table &#039;db_bimba.kelas&#039; doesn&#039;t exist in engine
+-- Kesalahan membaca data untuk tabel db_bimba.kelas: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `db_bimba`.`kelas`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `orang_tua`
+--
+-- Kesalahan membaca struktur untuk tabel db_bimba.orang_tua: #1932 - Table &#039;db_bimba.orang_tua&#039; doesn&#039;t exist in engine
+-- Kesalahan membaca data untuk tabel db_bimba.orang_tua: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `db_bimba`.`orang_tua`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `siswa`
+--
+-- Kesalahan membaca struktur untuk tabel db_bimba.siswa: #1932 - Table &#039;db_bimba.siswa&#039; doesn&#039;t exist in engine
+-- Kesalahan membaca data untuk tabel db_bimba.siswa: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `db_bimba`.`siswa`&#039; at line 1
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
